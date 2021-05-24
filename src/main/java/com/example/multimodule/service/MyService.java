ed8e378d8ee255a510405e2b21a8c73dc455f61c
yero0214 <yero0214@xensoft.co.kr-CLI-at-567502810040>
2021-05-24 10:59:27 +0900

    first commit

---
+++ src/main/java/com/example/multimodule/service/MyService.java
@@ -0,0 +1,11 @@
+package com.example.multimodule.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+	public String message() {
+		return "hello";
+	}
+}
